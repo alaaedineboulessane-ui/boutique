47b51cf2a639c78ee2cfd5e183a0e99b95965724
alaaedineboulessane-ui <alaaedine.boulessane@laplateforme.io>
2026-06-18 10:23:27 +0200

    reglage de bugs

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <link rel="stylesheet" href="./assets/css/Catalog.css">
 <link rel="stylesheet" href="./assets/css/cart.css">
 <link rel="stylesheet" href="./assets/css/Collection.css">
-<title>Wavey</title>
+<title>Wave</title>
 </head>
 <body>
 
